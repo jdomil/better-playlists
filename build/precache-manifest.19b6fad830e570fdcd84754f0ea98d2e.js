cb5247dd0b411f5477175a12dc183b600560bca7
juan <juan@stripe.com>
2018-10-08 23:36:14 +0100

    react-create-app on Heroku

---
+++ build/precache-manifest.19b6fad830e570fdcd84754f0ea98d2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b40edcf9a98b36e45d8b",
+    "url": "/static/js/main.b40edcf9.chunk.js"
+  },
+  {
+    "revision": "b8ee6f1af1a7111dbe38",
+    "url": "/static/js/1.b8ee6f1a.chunk.js"
+  },
+  {
+    "revision": "b40edcf9a98b36e45d8b",
+    "url": "/static/css/main.831fe2c1.chunk.css"
+  },
+  {
+    "revision": "f08d10cbb1c174d6d4aa859693e13a41",
+    "url": "/index.html"
+  }
+];
